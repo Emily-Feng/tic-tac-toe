c112a6174b4540733f1096ae5c3d782f579e4819
Emily Feng <manqingf@andrew.cmu.edu>
2021-05-28 17:44:44 -0400

    starter code

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import "./App.css";
+import Game from './Game';
+
+function App() {
+    return (<Game />);
+}
+
+export default App;
